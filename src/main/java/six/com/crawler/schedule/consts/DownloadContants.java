db9b60b2cde6f8b617e9530d157ae9cf0d5e1acb
Jonathan.Wei <weijiyonglover@gmail.com>
2017-04-25 10:34:37 +0800

    添加下载常亮类

---
+++ src/main/java/six/com/crawler/schedule/consts/DownloadContants.java
@@ -0,0 +1,19 @@
+package six.com.crawler.schedule.consts;
+
+/**
+ * 下载状态
+ * @author weijiyong@tospur.com
+ *
+ */
+public class DownloadContants {
+	
+	public static final int DOWN_LOAD_NOT_RUNNING=0;
+	
+	public static final int DOWN_LOAD_FINISHED = 1;
+	
+	public static final int DOWN_LOAD_RUNNING = 2;
+	
+	public static final int DOWN_LAOD_STOP = 3;
+	
+	public static final int DOWN_LOAD_EXCEPTION = 4;
+}
