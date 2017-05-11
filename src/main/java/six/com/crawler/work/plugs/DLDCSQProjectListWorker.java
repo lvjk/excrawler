196388569137f216f2806f5370b09ccb3c3b6951
Jonathan.Wei <weijiyonglover@gmail.com>
2017-05-11 17:23:22 +0800

    完成大连市内四区详细列表任务代码编写及测试

--- src/main/java/six/com/crawler/work/plugs/DLDCSQProjectListWorker.java
+++ src/main/java/six/com/crawler/work/plugs/DLDCSQProjectListWorker.java
@@ -1,19 +1,29 @@
 package six.com.crawler.work.plugs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import six.com.crawler.entity.Page;
 import six.com.crawler.entity.ResultContext;
 import six.com.crawler.http.HttpMethod;
+import six.com.crawler.utils.ArrayListUtils;
 import six.com.crawler.work.AbstractCrawlWorker;
 import six.com.crawler.work.WorkerLifecycleState;
 import six.com.crawler.work.space.WorkSpace;
 
+/**
+ * 
+ * @author weijiyong@tospur.com
+ *
+ */
 public class DLDCSQProjectListWorker extends AbstractCrawlWorker {
 
 	final static Logger log = LoggerFactory.getLogger(DLDCSQProjectListWorker.class);
@@ -26,11 +36,12 @@ public class DLDCSQProjectListWorker extends AbstractCrawlWorker {
 	String refererUrl;
 
 	private Page buildPage(int pageIndex, String refererUrl) {
-		Page page = new Page(getSite().getCode(), 1, PROJECT_LIST_URL, PROJECT_LIST_URL);
+		Page page = new Page(getSite().getCode(), 1, refererUrl, refererUrl);
 		page.setReferer(refererUrl);
 		page.setMethod(HttpMethod.POST);
-		page.getParameters().put("currentPage", pageIndex);
-		page.getParameters().put("pageSize", 10);
+		Map<String,Object> params=new HashMap<String,Object>();
+		params.put("pageNo", pageIndex);
+		page.setParameters(params);
 		return page;
 	}
 
@@ -38,7 +49,7 @@ public class DLDCSQProjectListWorker extends AbstractCrawlWorker {
 	protected void insideInit() {
 		projectInfoQueue = getManager().getWorkSpaceManager().newWorkSpace("dldc_sq_project_info", Page.class);
 		if (!(helper.isDownloadState() && helper.isUseRawData())) {
-			Page firstPage = buildPage(pageIndex, refererUrl);// 初始化第一页
+			Page firstPage = buildPage(pageIndex, PROJECT_LIST_URL);// 初始化第一页
 			getWorkSpace().clearDoing();
 			getWorkSpace().push(firstPage);
 		}
@@ -79,11 +90,19 @@ public class DLDCSQProjectListWorker extends AbstractCrawlWorker {
 	@Override
 	protected void onComplete(Page doingPage, ResultContext resultContext) {
 		// TODO Auto-generated method stub
-		List<String> projectInfoUrls = resultContext.getExtractResult("dldc_sq_project_info");
+		List<String> districts=new ArrayList<String>();
+		Elements trElements=doingPage.getDoc().select("table[bgcolor='#78abf1']>tbody>tr:gt(0)");
+		for (Element tr:trElements) {
+			Element district=tr.select("td[align='left']").first();
+			districts.add(district.ownText());
+		}
+		List<String> projectInfoUrls = resultContext.getExtractResult("projectUrl");
 		if (null != projectInfoUrls) {
-			for (String projectInfoUrl : projectInfoUrls) {
+			for (int i = 0; i < projectInfoUrls.size(); i++) {
+				String projectInfoUrl="http://www.dlfd.gov.cn"+projectInfoUrls.get(i);
 				Page projectInfo = new Page(getSite().getCode(), 1, projectInfoUrl, projectInfoUrl);
 				projectInfo.setReferer(doingPage.getFinalUrl());
+				projectInfo.getMetaMap().put("district", ArrayListUtils.asList(districts.get(i)));
 				projectInfoQueue.push(projectInfo);
 			}
 		}
