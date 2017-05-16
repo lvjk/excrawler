922877e525d09e6b84ba5e1b8fd30a31559ed419
Jonathan.Wei <weijiyonglover@gmail.com>
2017-05-16 16:02:10 +0800

    添加章丘预售、项目、楼栋、房间状态采集任务类

---
+++ src/main/java/six/com/crawler/work/plugs/ZQFGZXPresellInfoWorker.java
@@ -0,0 +1,99 @@
+package six.com.crawler.work.plugs;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.nodes.Element;
+
+import six.com.crawler.entity.Page;
+import six.com.crawler.entity.PageType;
+import six.com.crawler.entity.ResultContext;
+import six.com.crawler.http.HttpMethod;
+import six.com.crawler.utils.ArrayListUtils;
+import six.com.crawler.work.AbstractCrawlWorker;
+import six.com.crawler.work.space.WorkSpace;
+
+/**
+ * 
+ * @author weijiyong@tospur.com
+ *
+ */
+public class ZQFGZXPresellInfoWorker extends AbstractCrawlWorker{
+	
+	String pageIndexTemplate = "<<pageIndex>>";
+	String pageCountCss = "a[class=linktwo]";
+	int pageIndex = 1;
+	int pageCount = -1;
+	String projectListUrlTemplate = "http://www.zqfgzx.org.cn/index.php?s=/Property/ysxkgg/page/" + pageIndexTemplate+".html";
+	
+	WorkSpace<Page> unitInfoQueue;
+	
+	private Page buildPage(int pageIndex, String refererUrl) {
+		String pageUrl = StringUtils.replace(projectListUrlTemplate, pageIndexTemplate, String.valueOf(pageIndex));
+		Page page = new Page(getSite().getCode(), 1, pageUrl, pageUrl);
+		page.setReferer(refererUrl);
+		page.setMethod(HttpMethod.GET);
+		page.setType(PageType.LISTING.value());
+		return page;
+	}
+	
+	
+	@Override
+	protected void insideInit() {
+		
+		unitInfoQueue = getManager().getWorkSpaceManager().newWorkSpace("zqfgzx_unit_info", Page.class);
+		int pageIndex = 1;
+		Page firstPage = buildPage(pageIndex,null);// 初始化第一页
+		getDowner().down(firstPage);
+		
+		Element pageCountElement = firstPage.getDoc().select(pageCountCss).last();
+		String pageCountStr = StringUtils.substringBetween(pageCountElement.attr("href"), "/index.php?s=/Property/ysxkgg/page/",".html");
+		int pageCount = Integer.valueOf(pageCountStr);
+		
+		getWorkSpace().clearDoing();
+		
+		getWorkSpace().push(firstPage);
+		
+		Page lastPage = firstPage;
+		while (pageIndex <= pageCount) {
+			Page nextPage = buildPage(pageIndex, lastPage.getFinalUrl());// 初始化第一页
+			getWorkSpace().push(nextPage);
+			lastPage = nextPage;
+			pageIndex++;
+		}
+	}
+
+	@Override
+	protected void beforeDown(Page doingPage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void beforeExtract(Page doingPage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void afterExtract(Page doingPage, ResultContext resultContext) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onComplete(Page doingPage, ResultContext resultContext) {
+		String BASE_URL="http://www.zqfgzx.org.cn/";
+		List<String> unitIds=resultContext.getExtractResult("unitId");
+		List<String> unitUrls=resultContext.getExtractResult("unitUrl");
+		for (int i = 0; i < unitIds.size(); i++) {
+			String url=BASE_URL+unitUrls.get(i);
+			Page unitPage=new Page(doingPage.getSiteCode(), 1, url, url);
+			unitPage.setMethod(HttpMethod.GET);
+			unitPage.setReferer(doingPage.getReferer());
+			unitPage.getMetaMap().put("unitId", ArrayListUtils.asList(unitIds.get(i)));
+			
+			unitInfoQueue.push(unitPage);
+		}
+	}
+}
---
+++ src/main/java/six/com/crawler/work/plugs/ZQFGZXRoomStateInfoWorker.java
@@ -0,0 +1,127 @@
+package six.com.crawler.work.plugs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import six.com.crawler.entity.Page;
+import six.com.crawler.entity.ResultContext;
+import six.com.crawler.work.AbstractCrawlWorker;
+
+/**
+ * 
+ * @author weijiyong@tospur.com
+ *
+ */
+public class ZQFGZXRoomStateInfoWorker extends AbstractCrawlWorker{
+	
+	Map<String,String> roomState=new HashMap<String,String>();
+
+	@Override
+	protected void insideInit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void beforeDown(Page doingPage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void beforeExtract(Page doingPage) {
+		List<String> unitIds=new ArrayList<String>();
+		List<String> layers=new ArrayList<String>();
+		List<String> roomNos=new ArrayList<String>();
+		List<String> realEstates = new ArrayList<String>();
+		List<String> roomTypes = new ArrayList<String>();
+		List<String> realEstateNames = new ArrayList<String>();
+		List<String> preBuildAreas=new ArrayList<String>();
+		List<String> preShareAreas=new ArrayList<String>();
+		List<String> preInnerAreas=new ArrayList<String>();
+		List<String> roomProperties=new ArrayList<String>();
+		List<String> actBuildAreas=new ArrayList<String>();
+		List<String> actShareAreas=new ArrayList<String>();
+		List<String> actInnerAreas=new ArrayList<String>();
+		List<String> developers=new ArrayList<String>();
+		List<String> roomStates=new ArrayList<String>();
+		
+		String unitId=doingPage.getMeta("unitId").get(0);
+		
+		Elements floors=doingPage.getDoc().select("ul[class=RoomState]");
+		for (Element floor:floors) {
+			Elements rooms=floor.select("li:gt(0)");
+			for (Element room:rooms) {
+				Elements attrs=room.select("dl[class='loupan-info']>dd");
+				String realEstate=attrs.get(0).select("a").first().ownText();
+				String roomNo=attrs.get(1).ownText();
+				String roomType=attrs.get(2).ownText();
+				String roomProperty=attrs.get(8).ownText();
+				String layer=attrs.get(7).ownText();
+				
+				String realEstateName=attrs.get(6).select("a").first().ownText();
+				String preBuildArea=attrs.get(3).ownText();
+				String preShareArea=attrs.get(4).ownText();
+				String preInnerArea=attrs.get(5).ownText();
+				
+				String state=attrs.get(9).ownText();
+				String actBuildArea=attrs.get(10).ownText();
+				String actShareArea=attrs.get(11).ownText();
+				String actInnerArea=attrs.get(12).ownText();
+				String developer=attrs.get(13).ownText();
+				
+				preBuildAreas.add(preBuildArea);
+				preShareAreas.add(preShareArea);
+				preInnerAreas.add(preInnerArea);
+				
+				actBuildAreas.add(actBuildArea);
+				actShareAreas.add(actShareArea);
+				actInnerAreas.add(actInnerArea);
+				developers.add(developer);
+				roomProperties.add(roomProperty);
+				roomTypes.add(roomType);
+				realEstateNames.add(realEstateName);
+				realEstates.add(realEstate);
+				roomStates.add(state);
+				roomNos.add(roomNo);
+				layers.add(layer);
+				unitIds.add(unitId);
+			}
+		}
+		
+		doingPage.getMetaMap().put("unitId", unitIds);
+		doingPage.getMetaMap().put("layer", layers);
+		doingPage.getMetaMap().put("preBuildArea", preBuildAreas);
+		doingPage.getMetaMap().put("preShareArea", preShareAreas);
+		doingPage.getMetaMap().put("preInnerArea", preInnerAreas);
+		doingPage.getMetaMap().put("actBuildArea", actBuildAreas);
+		doingPage.getMetaMap().put("actShareArea", actShareAreas);
+		doingPage.getMetaMap().put("actInnerArea", actInnerAreas);
+		
+		doingPage.getMetaMap().put("roomNo", roomNos);
+		doingPage.getMetaMap().put("state", roomStates);
+		doingPage.getMetaMap().put("realEstateName", realEstates);
+		doingPage.getMetaMap().put("unitName", realEstateNames);
+		doingPage.getMetaMap().put("developer", developers);
+		doingPage.getMetaMap().put("roomType", roomTypes);
+		doingPage.getMetaMap().put("roomProperty", roomProperties);
+	}
+
+	@Override
+	protected void afterExtract(Page doingPage, ResultContext resultContext) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onComplete(Page doingPage, ResultContext resultContext) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
