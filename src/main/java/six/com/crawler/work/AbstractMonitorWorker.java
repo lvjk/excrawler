e75c1c4dc37e799a57941d2aa870a18a7fb14ecc
38134 <38134@SHZ3813406.tospur.com>
2017-06-06 11:50:24 +0800

    更新监控worker抽象类

--- src/main/java/six/com/crawler/work/AbstractMonitorWorker.java
+++ src/main/java/six/com/crawler/work/AbstractMonitorWorker.java
@@ -1,6 +1,7 @@
 package six.com.crawler.work;
 
 import six.com.crawler.entity.JobSnapshot;
+import six.com.crawler.work.exception.WorkerException;
 import six.com.crawler.work.space.Index;
 import six.com.crawler.work.space.WorkSpaceData;
 
@@ -14,12 +15,28 @@ import six.com.crawler.work.space.WorkSpaceData;
  */
 public abstract class AbstractMonitorWorker extends AbstractWorker<WorkSpaceData> {
 
+	/**
+	 * 被监控的任务名称
+	 */
+	private String targetJobName;
+
+	/**
+	 * 被监控的任务运行id
+	 */
+	private String targetJobSnapshotId;
+
 	public AbstractMonitorWorker() {
 		super(WorkSpaceData.class);
 	}
 
 	@Override
 	protected void initWorker(JobSnapshot jobSnapshot) {
+		targetJobName = jobSnapshot.getDispatchType().getName();
+		targetJobSnapshotId = jobSnapshot.getDispatchType().getCurrentTimeMillis();
+		fillWorkSpace();
+	}
+
+	private void fillWorkSpace() {
 		getWorkSpace().push(new WorkSpaceData() {
 			@Override
 			public void setIndex(Index index) {
@@ -35,5 +52,28 @@ public abstract class AbstractMonitorWorker extends AbstractWorker<WorkSpaceData
 				return null;
 			}
 		});
+	}
+
+	/**
+	 * 实现监控逻辑,需要循环监控的话，返回true,否则返回false监控任务线程将会结束
+	 * 
+	 * @return
+	 * @throws WorkerException
+	 */
+	protected abstract boolean doMonitor() throws WorkerException;
+
+	@Override
+	protected void insideWork(WorkSpaceData workerData) throws WorkerException {
+		if (doMonitor()) {
+			fillWorkSpace();
+		}
+	}
+
+	protected String getTargetJobName() {
+		return targetJobName;
+	}
+
+	protected String getTargetJobSnapshotId() {
+		return targetJobSnapshotId;
 	}
 }
